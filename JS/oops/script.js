f3e396c68cf1ef03298377beb390d5b7a7ccf471
Rahul Guarv <126690105+rahul428742@users.noreply.github.com>
2024-07-30 20:37:52 +0530

    work on edit delete button.

--- JS/oops/script.js
+++ JS/oops/script.js
@@ -1,8 +1,46 @@
-let arr = [1, 2, 3, 4];
-arr.sayHello = ()=>{console.log("hello from prototype..!")}
+// let arr = [1, 2, 3, 4];
+// arr.sayHello = ()=>{console.log("hello from prototype..!")}
 
-arr.__proto__.push = (n) => {
-    console.log("Pushing Element In Array ", n);
+// arr.__proto__.push = (n) => {
+//     console.log("Pushing Element In Array ", n);
+// }
+
+// console.log(Array.prototype === Array.prototype);
+
+
+// Constructor - doen't return anything. & Start with capital letter.
+// function Person(name, age) {
+//     this.name = name;
+//     this.age = age;
+// }
+// Person.__prototype__.talk = function (){
+//     console.log("Hii, Welcome", this.name);
+// }
+
+// let p1 = new Person("Rahul", 21);
+
+// class
+
+class Person{
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    talk() {
+        console.log("Hii, Welcome", this.name);
+    }
 }
 
-console.log(Array.prototype === Array.prototype);
+class Student extends Person {
+    constructor(name, age, marks) {
+        super(name, age); // parent class constructor is being cslled.
+        this.marks = marks;
+    }
+}
+
+class Teacher extends Person{
+    constructor(name, age, subject) {
+        super(name, age); // parent class constructor is being cslled.
+        this.subject = subject;
+    }
+}
