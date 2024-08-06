08c7d01783316a4b4d6aedc59b9cb47335ffe6e8
Rahul Guarv <126690105+rahul428742@users.noreply.github.com>
2024-08-06 22:32:47 +0530

    calc done

--- JS/PracticeProject/calc/script.js
+++ JS/PracticeProject/calc/script.js
@@ -1,21 +1,21 @@
-function appendToDisplay(value) {
-    document.getElementById('display').innerText += value;
+function appendToDisplay(value){
+    document.getElementById("display").innerText += value;
 }
 
 function clearDisplay() {
-    document.getElementById('display').innerText = '';
+    document.getElementById("display").innerText = "";
 }
 
-function backspace() {
-    let display = document.getElementById('display').innerText;
-    document.getElementById('display').innerText = display.slice(0, -1);
+function backSpace() {
+    let display = document.getElementById("display").innerText;
+    document.getElementById("display").innerText = display.slice(0, -1); 
 }
 
-function calculate() {
-    let display = document.getElementById('display').innerText;
+function Calculate() {
+    let display = document.getElementById("display").innerText;
     try {
-        document.getElementById('display').innerText = eval(display);
-    } catch {
-        document.getElementById('display').innerText = 'Error';
+        document.getElementById("display").innerText = eval(display);
+    } catch (error) {
+        document.getElementById("display").innerText = "error"
     }
 }
