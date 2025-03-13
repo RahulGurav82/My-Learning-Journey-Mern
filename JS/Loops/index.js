8b3a31fa9e9c0d54e2180459ca067f00dac15348
Rahul Guarv <126690105+rahul428742@users.noreply.github.com>
2025-03-13 20:41:56 +0530

    map

---
+++ JS/Loops/index.js
@@ -0,0 +1,10 @@
+// map it used to transform each elm in an array and a return a new array.
+
+const num = [1, 2, 3, 4, 5];
+
+let sq = num.map((num) => {
+    return num * num;
+});
+
+console.log(sq);
+
