060a3f18db880af3b16939f12338f874374055df
Rahul Guarv <126690105+rahul428742@users.noreply.github.com>
2025-03-13 21:05:23 +0530

    find & forEach

--- JS/Loops/index.js
+++ JS/Loops/index.js
@@ -1,11 +1,9 @@
 // map it used to transform each elm in an array and a return a new array.
 
 const num = [1, 2, 3, 4, 5];
-
 let sq = num.map((num) => {
     return num * num;
 });
-
 console.log(sq);
 
 // fillter used fillter elements from an array based on a condition
@@ -19,3 +17,19 @@ console.log(adults);
 const prices = [10, 20, 30, 40];
 const total = prices.reduce((sum, price) => sum + price, 0)
 console.log(total);
+
+// find used find the first element that matches a condition
+
+const users = [
+    {id : 1, name : "Alice"},
+    {id : 2, name : "Bob"},
+    {id : 3, name : "Charlie"}
+];
+
+const user = users.find(u => u.id === 2)
+console.log(user);
+
+// forEach used to loop over an array (does not return a new array).
+
+const fruits = ["Apple", "Bannana", "Mango"];
+fruits.forEach(fruit => console.log(fruit));
